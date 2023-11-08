571cba660101a845fa9dbd7369c89a6e572ea243
Jerson Júnior <jersonjunior@bsd.com.br>
2023-11-08 12:40:16 -0300

    Update rasa.py

--- rasa.py
+++ rasa.py
@@ -1,9 +1,1 @@
-import requests
-import json
-r = requests.post('http://127.0.0.1:5005/webhooks/rest/webhook', verify=False,
-#data=json.dumps({"sender":"5561998002190","message":"escolho informações"}),
-#data=json.dumps({"sender":"5561998002192", "message":"/beneficio_seguro_bf_suspenso"}),
-data=json.dumps({"sender":"5561998002192", "message":"A restrição específica foi encerrada."}),
-headers={'Accept': 'application/json', 'Content-Type': 'application/json'})
-#r.encoding = 'utf-8z-sig'
-resposta = json.loads(r.text)
+031865561
